ee3da5e7cc3bb57bcee54a63ba1cff0d4e5f585d
weezard12 <weezard1234@gmail.com>
2025-07-16 16:33:10 +0300

    Fixed nez errors. Got to Nez black screen

--- WEngine/Game1.cs
+++ WEngine/Game1.cs
@@ -1,52 +1,36 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Nez;
 
 namespace WEngine
 {
-    public class Game1 : Game
+    public class Game1 : Core
     {
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
-
         public Game1()
         {
-            _graphics = new GraphicsDeviceManager(this);
-            Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
             base.Initialize();
-        }
 
-        protected override void LoadContent()
-        {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
+            // TODO: Add your initialization logic here
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
+            base.Update(gameTime);
 
             // TODO: Add your update logic here
-
-            base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            base.Draw(gameTime);
 
             // TODO: Add your drawing code here
-
-            base.Draw(gameTime);
         }
     }
 }
