546ce00b335810bdbe9a101614314d7f258e9ed7
weezard12 <weezard1234@gmail.com>
2025-07-17 15:59:12 +0300

    Removed incorrect text

--- Nez/Nez.Portable/Assets/BitmapFonts/BitmapFont.cs
+++ Nez/Nez.Portable/Assets/BitmapFonts/BitmapFont.cs
@@ -1,18 +1,11 @@
-﻿using Nez;
-using Nez.UI;
-
-I want to add a Simple UI to my Editor with Nez UI System. Each UI part will have a name and a border around it.
-Is this base class correct?
-
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Nez;
 using Nez.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using WEngine.Scripts.Main;
+
 
 namespace WEngine.Scripts.GameLogic.TilesEditor.Base
 {
