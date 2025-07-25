408f00763da3b5fa8fe9c2fd181ca700ff650bf9
weezard12 <weezard1234@gmail.com>
2025-07-25 13:59:18 +0300

    changing search pos

--- WEngine/Scripts/GameLogic/Gum/GumProjectUI/Components/MyComponents/FilesViewer.Generated.cs
+++ WEngine/Scripts/GameLogic/Gum/GumProjectUI/Components/MyComponents/FilesViewer.Generated.cs
@@ -34,6 +34,7 @@ partial class FilesViewer : MonoGameGum.Forms.Controls.FrameworkElement
     public ScrollViewer ScrollViewerInstance { get; protected set; }
     public TextBox PathTextBox { get; protected set; }
     public TextBox SearchTextBox { get; protected set; }
+    public Panel PanelInstance { get; protected set; }
 
     public FilesViewer(InteractiveGue visual) : base(visual) { }
     public FilesViewer()
@@ -48,6 +49,7 @@ partial class FilesViewer : MonoGameGum.Forms.Controls.FrameworkElement
         ScrollViewerInstance = MonoGameGum.Forms.GraphicalUiElementFormsExtensions.TryGetFrameworkElementByName<ScrollViewer>(this.Visual,"ScrollViewerInstance");
         PathTextBox = MonoGameGum.Forms.GraphicalUiElementFormsExtensions.TryGetFrameworkElementByName<TextBox>(this.Visual,"PathTextBox");
         SearchTextBox = MonoGameGum.Forms.GraphicalUiElementFormsExtensions.TryGetFrameworkElementByName<TextBox>(this.Visual,"SearchTextBox");
+        PanelInstance = MonoGameGum.Forms.GraphicalUiElementFormsExtensions.TryGetFrameworkElementByName<Panel>(this.Visual,"PanelInstance");
         CustomInitialize();
     }
     //Not assigning variables because Object Instantiation Type is set to By Name rather than Fully In Code
