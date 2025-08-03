b9b75414a8d918012fcbba810693e136522efa95
weezard12 <weezard1234@gmail.com>
2025-08-03 20:08:17 +0300

    using the correct point class

--- WEngine/Scripts/GameLogic/Tiles/TilesLayer.cs
+++ WEngine/Scripts/GameLogic/Tiles/TilesLayer.cs
@@ -1,7 +1,7 @@
-﻿using Nez;
+﻿using Microsoft.Xna.Framework;
+using Nez;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
