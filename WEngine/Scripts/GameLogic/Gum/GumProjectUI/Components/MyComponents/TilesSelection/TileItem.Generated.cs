1701dd00a8cc3ce244928bf4e27a2ccea9fd183d
weezard12 <weezard1234@gmail.com>
2025-08-15 16:55:18 +0300

    working on selecting tiles

--- WEngine/Scripts/GameLogic/Gum/GumProjectUI/Components/MyComponents/TilesSelection/TileItem.Generated.cs
+++ WEngine/Scripts/GameLogic/Gum/GumProjectUI/Components/MyComponents/TilesSelection/TileItem.Generated.cs
@@ -31,11 +31,44 @@ partial class TileItem : MonoGameGum.Forms.Controls.FrameworkElement
             return gue;
         });
     }
+    public enum QuickStyles
+    {
+        Hovered,
+        Selected,
+        Clear,
+    }
+
+    QuickStyles? _quickStylesState;
+    public QuickStyles? QuickStylesState
+    {
+        get => _quickStylesState;
+        set
+        {
+            _quickStylesState = value;
+            if(value != null)
+            {
+                if(Visual.Categories.ContainsKey("QuickStyles"))
+                {
+                    var category = Visual.Categories["QuickStyles"];
+                    var state = category.States.Find(item => item.Name == value.ToString());
+                    this.Visual.ApplyState(state);
+                }
+                else
+                {
+                    var category = ((Gum.DataTypes.ElementSave)this.Visual.Tag).Categories.FirstOrDefault(item => item.Name == "QuickStyles");
+                    var state = category.States.Find(item => item.Name == value.ToString());
+                    this.Visual.ApplyState(state);
+                }
+            }
+        }
+    }
     public SpriteRuntime TileSprite { get; protected set; }
     public CustomTextBox TileName { get; protected set; }
     public Label TileType { get; protected set; }
     public ContainerRuntime SpriteContainer { get; protected set; }
     public Panel PanelInstance { get; protected set; }
+    public ColoredRectangleRuntime ColoredRectangleInstance { get; protected set; }
+    public ContainerRuntime ContainerInstance { get; protected set; }
 
     public TileItem(InteractiveGue visual) : base(visual)
     {
@@ -54,6 +87,8 @@ partial class TileItem : MonoGameGum.Forms.Controls.FrameworkElement
         TileType = MonoGameGum.Forms.GraphicalUiElementFormsExtensions.TryGetFrameworkElementByName<Label>(this.Visual,"TileType");
         SpriteContainer = this.Visual?.GetGraphicalUiElementByName("SpriteContainer") as ContainerRuntime;
         PanelInstance = MonoGameGum.Forms.GraphicalUiElementFormsExtensions.TryGetFrameworkElementByName<Panel>(this.Visual,"PanelInstance");
+        ColoredRectangleInstance = this.Visual?.GetGraphicalUiElementByName("ColoredRectangleInstance") as ColoredRectangleRuntime;
+        ContainerInstance = this.Visual?.GetGraphicalUiElementByName("ContainerInstance") as ContainerRuntime;
         CustomInitialize();
     }
     //Not assigning variables because Object Instantiation Type is set to By Name rather than Fully In Code
